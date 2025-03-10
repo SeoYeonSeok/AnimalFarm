bba51b48c6373ab89084da2a5bedc106ae932bc4
SeoYeonSeok <80459151+SeoYeonSeok@users.noreply.github.com>
2025-03-10 12:56:21 +0900

    250310
    Adding Simple SignIn/LogIn Btn & InputField

---
+++ Assets/02.Scripts/SYS/Bar.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    public Image bar;
+    public TextMeshProUGUI label;
+    public TextMeshProUGUI barValue;
+}
